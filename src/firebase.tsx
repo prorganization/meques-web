f28717566d2aa18c9c32c4dcd2198f72520a07f4
paulrusu94 <paul.rusu94@gmail.com>
2023-02-27 13:02:57 +0200

    Added .env file.

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCsAVK8C19zK409gAiwbg84tHf2uT6rFL4",
-    authDomain: "meques-6925c.firebaseapp.com",
-    projectId: "meques-6925c",
-    storageBucket: "meques-6925c.appspot.com",
-    messagingSenderId: "174450290932",
-    appId: "1:174450290932:web:0bba90e3b09e08a58baea1",
-    measurementId: "G-T4YD2XHK7L"
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
